f45949c0ef5fa7422ccf2d190eb3ef700989a2bb
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2018-11-28 14:14:01 +0900

    研究室情報の出力を作成しました。

---
+++ modeling_ateam/ThirdexViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdexViewController.swift
+//  modeling_ateam
+//
+//  Created by Shinjo Takuya on 2018/11/28.
+//  Copyright © 2018 yusuke minei. All rights reserved.
+//
+
+import UIKit
+
+class ThirdexViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
