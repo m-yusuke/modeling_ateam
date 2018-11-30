2b100e492e1e717abd0777b3fd0b23ab35c633f7
Yusuke Minei <e175703@ie.u-ryukyu.ac.jp>
2018-11-30 23:28:26 +0900

    各研究室ページへの画面遷移を実装し, LabInfo.storyboardのページの全ボードにStoryboardIDを割り振った

--- modeling_ateam/NextViewController.swift
+++ modeling_ateam/NextViewController.swift
@@ -12,14 +12,14 @@ class NextViewController: UIViewController, UISearchBarDelegate, UITableViewDele
     @IBOutlet weak var tableView: UITableView!
     
     // データ操作用の配列
-    private var array: NSArray = []
+    private var array:NSArray = []
     // 出力用の配列
-    private var outputArrays: NSArray = []
+    private var outputArrays:NSArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        array = ["akamine","kono","nagatani","nagayama","yosida","nal","nakamura","kunita","kang","endo","tamaki","yamada","okazaki","miyazato","wada"]
+        array = ["akamine","kono","nagatani","nagayama","yoshida","nal","nakamura","kunita","kang","endo","tamaki","yamada","okazaki","miyazato","wada"]
         
         outputArrays = array
         
@@ -55,8 +55,62 @@ class NextViewController: UIViewController, UISearchBarDelegate, UITableViewDele
     // 画面遷移の処理もここで書いている
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        var boardID : String = ""
+        
+        switch self.outputArrays[indexPath.row] as? String {
+            case "akamine":
+                print("Akamine")
+                boardID = "Akamine"
+            case "kono":
+                print("Kono")
+                boardID = "Kono"
+            case "nagatani":
+                print("Nagatanien")
+                boardID = "Nagatanien"
+            case "nagayama":
+                print("Nagayama")
+                boardID = "Nagayama"
+            case "yoshida":
+                print("Yoshida")
+                boardID = "Yoshida"
+            case "nal":
+                print("Nal")
+                boardID = "Nal"
+            case "nakamura":
+                print("Nakamura")
+                boardID = "Nakamura"
+            case "kunita":
+                print("Kunita")
+                boardID = "Kunita"
+            case "kang":
+                print("Kang")
+                boardID = "Kang"
+            case "endo":
+                print("Endo")
+                boardID = "Endo"
+            case "tamaki":
+                print("Tamaki")
+                boardID = "Tamaki"
+            case "yamada":
+                print("Yamada")
+                boardID = "Yamada"
+            case "okazaki":
+                print("Okazaki")
+                boardID = "Okazaki"
+            case "miyazato":
+                print("Miyazato")
+                boardID = "Miyazato"
+            case "wada":
+                print("Wada")
+                boardID = "Wada"
+            case .none:
+                break
+            case .some(_):
+                break
+        }
+        
         let storyboard: UIStoryboard = UIStoryboard(name: "LabInfo", bundle: nil)
-        let labView = storyboard.instantiateViewController(withIdentifier:"Nagatanien") as! LabViewController
+        let labView = storyboard.instantiateViewController(withIdentifier:boardID) as! LabViewController
         self.present(labView, animated: true, completion: nil)
     }
     
