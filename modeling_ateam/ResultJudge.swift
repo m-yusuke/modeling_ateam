c3ef2accbc704cb76ef864083c228591417ec569
Takuya Shinjo <e175769@ie.u-ryukyu.ac.jp>
2019-01-24 10:44:28 +0900

    研究室診断結果の修正

--- modeling_ateam/ResultJudge.swift
+++ modeling_ateam/ResultJudge.swift
@@ -2,9 +2,29 @@
 //  ResultJudge.swift
 //  modeling_ateam
 //
-//  Created by Shinjo Takuya on 2019/01/23.
+//  Created by Shinjo Takuya on 2019/01/24.
 //  Copyright © 2019 yusuke minei. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
+class ResultJudge: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
